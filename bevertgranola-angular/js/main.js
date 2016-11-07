47457047569059959f950ddc89ba7c6da0b65211
Blaze31 <bertrandgarnier31@gmail.com>
2016-11-07 01:32:56 -0500

    adding snipcart

--- bevertgranola-angular/js/main.js
+++ bevertgranola-angular/js/main.js
@@ -4,8 +4,8 @@ var app = angular.module('beVertGranolaApp', [
 ])
 .factory('productService', function () {
         var selectedProduct = '';
-        var productsList = [{id: 1, img:'../images/sac_02.jpg',title:'Amandes & coco', price:'12.00 $'},
-                            {id: 2,img:'../images/sac_02.jpg',title:'Érables & pacanes', price:'12.00 $'}];
+        var productsList = [{id: 1, img:'../images/sac_02.jpg',ing:"../images/vn_01.jpg", title:'Amandes & coco', price:'12.00 $',weight:'300',url:'https://bevertgranola-406c1.firebaseapp.com/',description:'Amandes & coco'},
+                            {id: 2,img:'../images/sac_02.jpg',ing:"../images/vn_02.jpg", title:'Érables & pacanes', price:'12.00 $',weight:'300',url:'https://bevertgranola-406c1.firebaseapp.com/',description:'Érables & pacanes'}];
 
         return {
             getSelectedProduct: function () {
@@ -29,19 +29,30 @@ app.config(['$locationProvider','$routeProvider', function ($locationProvider,$r
   $routeProvider
     // Home
     .when("/", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/!/", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/about!/", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/store", {templateUrl: "partials/store.html", controller: "PageCtrl"})
+    .when("/store!/", {templateUrl: "partials/store.html", controller: "PageCtrl"})
     .when("/store/:id", {templateUrl: "partials/product.html", controller: "PageCtrl"})
     // else 404
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+    //.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
 app.controller('PageCtrl', function ( $scope, $location, $http ) {
-  setActiveLink($location.path().replace("/",""));
-  
-  
-  function setActiveLink(path){
+  var path = $location.path();
+  setActiveLink(path.replace("/",""),$location);
+  $('#hidden_1').hide();
+  $('#hidden_2').hide();
+
+  function setActiveLink(path,$location){
+    if(path.indexOf('!') > -1){
+      var pos = path.indexOf('!');
+      var newPath = path.substring(0,pos);
+      path = newPath
+      $location.path(newPath);
+    }
     if(path == "" || path=='/'){
       path ='about';
     }
@@ -49,6 +60,7 @@ app.controller('PageCtrl', function ( $scope, $location, $http ) {
       path = 'store';
     }
     var id = "#link_" + path;
+    $(".nav").find(".active").removeClass("active");
     $(".nav").find(id).addClass("active");
   }
 });
