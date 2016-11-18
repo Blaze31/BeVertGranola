199a7f01d9a81062bb95724aef29069b2c8c88a7
Blaze31 <bertrandgarnier31@gmail.com>
2016-11-18 00:45:31 -0500

    Fixed top nav bar top + redirect to store button

--- bevertgranola-angular/js/product.js
+++ bevertgranola-angular/js/product.js
@@ -2,9 +2,10 @@ app.controller('productCtrl',['$scope','$location','productService', function ($
     $(".nav").find(".active").removeClass("active");
     $(".nav").find("#link_store").addClass("active");
     var productId = Number($location.path()[$location.path().length - 1]);
-    console.log("productId:  " + productId);
-    console.log(productService.getProductId(productId));
     $scope.selectedProduct = productService.getProductId(productId);
+    $scope.redirectToStore = function(){
+        $location.path("/store");
+    }
     /*if($scope.selectedProduct == undefined){
         $location.path('/store');
     }*/
