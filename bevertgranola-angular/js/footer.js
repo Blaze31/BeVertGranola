8e132b78fc23dbc1f7b7ffebce16614bc97401ad
Blaze31 <bertrandgarnier31@gmail.com>
2016-11-27 23:18:23 -0500

    Ajout du Footer et fix du styling du store

---
+++ bevertgranola-angular/js/footer.js
@@ -0,0 +1,7 @@
+app.directive('footer', function(){
+    return {
+        templateUrl: "../templates/footer.html",
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+        }]
+    }
+});
