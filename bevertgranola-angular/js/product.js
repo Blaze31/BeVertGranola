d95c98ca709c27e2adf65dfb4b362714798c674d
Blaze31 <bertrandgarnier31@gmail.com>
2016-11-28 01:08:52 -0500

    Ajouted list images

--- bevertgranola-angular/js/product.js
+++ bevertgranola-angular/js/product.js
@@ -3,6 +3,9 @@ app.controller('productCtrl',['$scope','$location','productService', function ($
     $(".nav").find("#link_store").addClass("active");
     var productId = Number($location.path()[$location.path().length - 1]);
     $scope.selectedProduct = productService.getProductId(productId);
+    $scope.displayProductImage = $scope.selectedProduct.img;
+    $scope.listImageDetails = getDetailsImages($scope.selectedProduct.detailsimages);
+    $scope.showImageDetail = $scope.listImageDetails.length > 0?true:false;
     $scope.quantity = 1;
     $scope.redirectToStore = function(){
         $location.path("/store");
@@ -13,5 +16,22 @@ app.controller('productCtrl',['$scope','$location','productService', function ($
     }
     $scope.toggleModal = function(){
         $('#checkoutModal').modal('toggle');
+    }
+    $scope.setDisplayImage = function(image){
+        $scope.displayProductImage = image.src;
+    }
+    function getDetailsImages(listImageSrcString){
+        console.log(listImageSrcString == "");
+        var returnListImagesObject = [];
+        if(listImageSrcString == ""){
+            return returnListImagesObject;
+        }
+        var listImageSrcArray = listImageSrcString.split(',');
+        for(var i=0;i < listImageSrcArray.length;++i){
+            var id = i;
+            var obj = {id:id,src:listImageSrcArray[i]};
+            returnListImagesObject.push(obj);
+        }
+        return returnListImagesObject;
     }
 }]);
